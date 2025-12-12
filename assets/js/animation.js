44abd1d197b8b24a32cc6d5a0b67cf81c8448e6b
Faiz Ansari <faizansari2025@gmail.com>
2025-12-12 13:14:36 -0800

    add companies code

---
+++ assets/js/animation.js
@@ -0,0 +1,67 @@
+
+// GSAP plugin register
+gsap.registerPlugin(ScrollTrigger);
+
+/* ============================
+   1. Service Cards Animation
+=============================== */
+gsap.from(".herosection", {
+  scrollTrigger: {
+    trigger: ".herosection",
+    start: "top 80%",
+    toggleActions: "play none none reverse"
+  },
+  opacity: 0,
+  y: 50,
+  duration: 1,
+  stagger: 0.2,
+  ease: "power3.out"
+});
+
+
+/* ============================
+   2. Why Choose Us Boxes
+=============================== */
+gsap.from(".about", {
+  scrollTrigger: {
+    trigger: ".about",
+    start: "top 80%",
+    toggleActions: "play none none reverse"
+  },
+  opacity: 0,
+  y: 40,
+  duration: 1,
+  stagger: 0.2,
+  ease: "power3.out"
+});
+gsap.from(".services", {
+  scrollTrigger: {
+    trigger: ".services",
+    start: "top 80%",
+    toggleActions: "play none none reverse"
+  },
+  opacity: 0,
+  y: 40,
+  duration: 1,
+  stagger: 0.2,
+  ease: "power3.out"
+});
+gsap.from(".whychose", {
+  scrollTrigger: {
+    trigger: ".whychose",
+    start: "top 80%",
+    toggleActions: "play none none reverse"
+  },
+  opacity: 0,
+  y: 40,
+  duration: 1,
+  stagger: 0.2,
+  ease: "power3.out"
+});
+
+
+
+
+
+
+
